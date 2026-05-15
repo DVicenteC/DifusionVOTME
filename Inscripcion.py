60f72a4d3135b8479b15b7f44b5cf491db0e04e2
Diego Vicente <diego.vicente@ug.uchile.cl>
2026-05-15 10:48:31 -0400

    feat: update registration form title to include training details and schedule

--- Inscripcion.py
+++ Inscripcion.py
@@ -471,7 +471,7 @@ try:
 
     # Mostrar formulario de inscripción
     try:
-        st.title("Registro de asistencia Capacitación Interna IST - Protocolo Psicosocial (CEAL)")
+        st.title("Registro de asistencia Capacitación Interna IST - Protocolo Psicosocial (CEAL) - Prevención de Suicidio – 11:15 a 13:15 pm.")
 
         # Obtener todos los cursos
         df_cursos = get_config_data()
